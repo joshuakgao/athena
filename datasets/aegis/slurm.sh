700dec1e898c803fec1da459420d7df768a35b44
Joshua Gao <joshuakgao@gmail.com>
2025-04-02 12:02:20 -0500

    Show memory availbility

--- datasets/aegis/slurm.sh
+++ datasets/aegis/slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH -o athena.o%j
 #SBATCH --ntasks-per-node=1 -N 1
 #SBATCH -t 5-96:0:0
-#SBATCH --mem-per-cpu=128GB
+#SBATCH --mem-per-cpu=256GB
 
 module add Miniforge3/py3.10
 module add cudatoolkit/12.4
@@ -13,4 +13,5 @@ export CONDA_PKGS_DIRS=/project/hoskere/jkgao/.conda/conda_pkgs_dir/
 export XDG_CACHE_HOME=/project/hoskere/jkgao/.conda/cache/
 export PYTHONPATH=/project/hoskere/jkgao/.conda/envs/athena/bin/python
 
+free -h
 /project/hoskere/jkgao/.conda/envs/athena/bin/python -m datasets.aegis.generate
