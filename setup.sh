79f782d8a960a9015b2fad95935b94d44a483c0b
tugonbob <57576522+tugonbob@users.noreply.github.com>
2025-04-30 15:37:45 -0500

    Prep for chessbench dataset

--- setup.sh
+++ setup.sh
@@ -2,18 +2,5 @@
 pip install -r requirements.txt
 pip install -r lichess-bot/requirements.txt
 
-# Install stockfish
-OS=$(uname -s) # Get the OS name
-if [ "$OS" = "Linux" ]; then
-    sudo apt install stockfish
-elif [ "$OS" = "Darwin" ]; then
-    brew stockfish
-else
-    echo "This is an unknown or unsupported system: $OS"
-fi
-
-# Download lichess pgn chess game databases
-# You can go into download.sh to change which size datasets you want
-bash datasets/lichess/download.sh
-echo "Parsing chess database. This may take a while..."
-python -m datasets.lichess.generate
+# Download rosbag
+sudo apt install python3-rosbag
